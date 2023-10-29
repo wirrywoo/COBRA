f3211f489871cc8b26e6ef9cbb8e05740b8d5d3b
Wilson Cheung <info@wilsoncheung.me>
2023-10-29 09:37:00 +0000

    Incorporated static files for A/B Testing design

--- django2/demo/urls.py
+++ django2/demo/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('split-test/', TemplateView.as_view(template_name="index.html")),
-]
+] + staticfiles_urlpatterns()
